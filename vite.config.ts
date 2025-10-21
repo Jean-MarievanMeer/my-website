d4328f2c3610779fb28a3717bc1d5a5d6e6de8fe
Jean-MarievanMeer <jeanmarievmeer@gmail.com>
2025-10-21 13:03:46 +0200

    SSR -> SPA

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,19 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const repoName = "my-website";
+
 export default defineConfig({
+
+  base: process.env.NODE_ENV === 'production' 
+    ? `/${repoName}/` 
+    : '/',
+
   plugins: [tailwindcss(), reactRouter(), 
   tsconfigPaths()],
+
+  build: {
+    // Ensures the final files are in the 'dist' folder
+    outDir: 'dist', 
+  },
 });
