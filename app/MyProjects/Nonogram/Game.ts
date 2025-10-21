130ea94c874960630382befb243fc69328aad102
Jean-MarievanMeer <jeanmarievmeer@gmail.com>
2025-10-21 16:02:14 +0200

    404-redirect

--- app/MyProjects/Nonogram/Game.ts
+++ app/MyProjects/Nonogram/Game.ts
@@ -1,7 +1,17 @@
+import { useSearchParams } from 'react-router';
 
+const Game = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const id = setID();
 
-const id = setID();
+    function setID() {
+        let id = searchParams.get("id");
 
-function setID(){
-    
+        if (!id) {
+            id = Date.now().toString();
+            setSearchParams({ id: id.toString() });
+        }
+
+        return id;
+    }
 }
